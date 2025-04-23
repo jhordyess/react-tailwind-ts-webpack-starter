87661d91685ff2f4494ac7440891b5faff249e2b
jhordyess <dev@jhordyess.com>
2025-04-23 10:25:28 -0400

    Add React Hooks ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,24 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import { defineConfig } from 'eslint/config'
+import reactHooks from 'eslint-plugin-react-hooks'
 
 export default defineConfig([
+  { ignores: ['node_modules', 'dist'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
 ])
