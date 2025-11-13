d4b082658daa7d0c6c3747de98834b1cde5664d3
jhordyess <65508346+jhordyess@users.noreply.github.com>
2025-11-13 16:45:52 -0400

    chore: Refactor eslint configuration for improved readability and structure

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,23 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
 import reactHooks from 'eslint-plugin-react-hooks'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   { ignores: ['node_modules', 'dist'] },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser }
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooks.configs.flat['recommended-latest'],
+  /* Custom Rules for eslint-plugin-react */
   {
-    plugins: { 'react-hooks': reactHooks },
     rules: {
-      ...reactHooks.configs['recommended-latest'].rules,
       'react/react-in-jsx-scope': 'off'
     },
     settings: {
